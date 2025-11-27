aab51b511b7ef14909f7c2b3b64dc59e5d34098f
Eddie Naff <eddienaff@gmail.com>
2025-11-27 13:17:29 -0800

    Add create db script

---
+++ src/db/create_db.sql
@@ -0,0 +1,5 @@
+-- Source - https://stackoverflow.com/a/76640843
+-- Posted by Daniel S., modified by community. See post 'Timeline' for change history
+-- Retrieved 2025-11-27, License - CC BY-SA 4.0
+
+CREATE DATABASE todo_test;
